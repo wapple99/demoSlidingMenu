4607e0011cc0adba2f65749b3d45357a2b949fca
wapple99 <wapple99@163.com>
2015-10-22 17:33:07 +0800

    add some code

--- app/src/main/java/com/bruce/slidingmenueasy/MainActivity.java
+++ app/src/main/java/com/bruce/slidingmenueasy/MainActivity.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
+
+    private SlidingMenu mMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMenu = (SlidingMenu) findViewById(R.id.id_menu);
     }
 
     @Override
@@ -34,5 +38,10 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void toggleMenu(View view)
+    {
+        mMenu.toggle();
     }
 }
